6a126133f8a9469373a65a4d97b6650a39e6d113
Michael Hope Chukwu <78309364+ujuhope@users.noreply.github.com>
2023-09-18 10:39:02 +0100

    made some changes

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import peer from "simple-peer";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import peer from "simple-peer";
+import { io } from "socket.io-client";
+
+const socket = io("https://connect-me-47177b2104fc.herokuapp.com");
+
+export default socket;
