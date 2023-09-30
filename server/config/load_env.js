60c9518a8f3f8be36eda18b7b7931355a5e7587a
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-09-30 11:57:12 +0200

    fix server errors

--- server/config/load_env.js
+++ server/config/load_env.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const config = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 5000,
 };
 
 export default config;
--- server/index.js
+++ server/index.js
@@ -22,6 +22,45 @@ app.get('/', (req, res) => {
   res.send('Server is running successfully');
 });
 
-server.listen(config.port, () => console.log(`Server is running on port ${config.port}`));
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+  
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", {
+      signal: signalData,
+      from,
+      name,
+    });
+  });
 
-export default io;
+  socket.on("updateMyMedia", ({ type, currentMediaStatus }) => {
+    console.log("updateMyMedia");
+    socket.broadcast.emit("updateUserMedia", { type, currentMediaStatus });
+  });
+
+  socket.on("msgUser", ({ name, to, msg, sender }) => {
+    io.to(to).emit("msgRcv", { name, msg, sender });
+  });
+
+  socket.on('sendTypingSignal', ({ to, sender }) => {
+    socket.broadcast.emit('rcvTypingSignal', { to, sender });
+  });
+
+  socket.on('sendStopTypingSignal', ({ to, sender }) => {
+    socket.broadcast.emit('rcvStopTypingSignal', { to, sender });
+  });
+
+  socket.on("answerCall", (data) => {
+    socket.broadcast.emit("updateUserMedia", {
+      type: data.type,
+      currentMediaStatus: data.myMediaStatus,
+    });
+    io.to(data.to).emit("callAccepted", data);
+  });
+
+  socket.on("endCall", ({ id }) => {
+    io.to(id).emit("endCall");
+  });
+});
+
+server.listen(config.port, () => console.log(`Server is running on port ${config.port}`));
