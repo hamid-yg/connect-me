b37f461b53e1f55998fecfbe853b2a52d62ba946
Michael Hope Chukwu <78309364+ujuhope@users.noreply.github.com>
2023-09-30 09:14:26 +0100

    made some changes

--- server/socket/call.js
+++ server/socket/call.js
@@ -1,5 +1,32 @@
 // Call with Socket.io instance
 
+// import io from "../index";
+
+// io.on("connection", (socket) => {
+//   socket.emit("me", socket.id);
+
+//   socket.on("disconnect", () => {
+//     socket.broadcast.emit("callEnded");
+//   });
+
+//   socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+//     io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+//   });
+
+//   socket.on("answerCall", (data) => {
+//     socket.broadcast.emit("updateUserMedia", {
+//       type: data.type,
+//       currentMediaStatus: data.myMediaStatus,
+//     });
+//     io.to(data.to).emit("callAccepted", data);
+//   });
+
+//   socket.on("endCall", ({ id }) => {
+//     io.to(id).emit("endCall");
+//   });
+// });
+
+
 import io from "../index";
 
 io.on("connection", (socket) => {
@@ -14,15 +41,9 @@ io.on("connection", (socket) => {
   });
 
   socket.on("answerCall", (data) => {
-    socket.broadcast.emit("updateUserMedia", {
-      type: data.type,
-      currentMediaStatus: data.myMediaStatus,
-    });
-    io.to(data.to).emit("callAccepted", data);
+    io.to(data.to).emit("callAccepted", data.signal)
+    
+  
   });
-
-  socket.on("endCall", ({ id }) => {
-    io.to(id).emit("endCall");
-  });
-});
-
+ })
+  
