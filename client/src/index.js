d9bbd5a3344516e8395ce78abf0f23f30ff15333
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-10-06 15:57:28 +0200

    remove service worker

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "./serviceWorker";
+// import reportWebVitals from "./reportWebVitals";
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,5 +12,5 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-reportWebVitals();
-serviceWorker.register();
+// reportWebVitals();
+// serviceWorker.register();
